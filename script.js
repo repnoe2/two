226d292ffb7a6cfa68132db2f1d905ba5b690576
repnoe2 <115735429+repnoe2@users.noreply.github.com>
2023-02-07 18:59:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+/*
+let name = prompt ('ведите пароль');
+alert (' проверь себя! ');
+if(true){
+    alert (' ответ: спосибо мы тебя взломаем!!!! ');
+
+
+}
+let namy = prompt ('ведите номер карты!!!');
+alert (' проверь себя! ');
+if(true){
+    alert (' ответ: 9361 4509 2197 3688 ');
+}
+let nas = prompt ('сколько вам лет?');
+if (nas < 18 ) {
+    alert ('ты карапуз!!! ха');
+} else if (nas > 18) {
+alert ('ты такой старый?');
+} else {
+    alert('ты норм !')
+}
+let nasp = prompt ('твой любимый ютубер?');
+if (nasp == 'эдисон') {
+    alert ('правильно!');
+    alert ('ты гений!!');
+} else {
+alert ('нет!');
+}
+let nasl = prompt ('твоя любимая игра?');
+if (nasl == 'майнкрафт') {
+    alert ('правильно!');
+    alert ('ты гений!!');
+} else {
+alert ('чел скачай майнкрафт!');
+}
+let nasa = prompt ('сколько ты хочеш денег ?');
+/*   if (nas < 1000000 ) {
+    alert ('слишком мало!');
+} else if (nas > 1000000) {
+alert ('слишком много');
+} else {
+    alert('ты выйграл милион!!')
+}      
+
+while(nasa<1000000){
+nasa = prompt('слишком мало, введите ещё!');
+}
+alert('отлично!!! ты выиграл милион!!!')
+
+*/
+/*
+let a = prompt("Выберите цвет?");
+if (a==1) { 
+    document.body.style.background="white";
+    alert("Вы выбрали белый!")
+}
+if (a==2) { 
+document.body.style.background="red";
+alert("Вы выбрали красный!")
+}
+if (a==3) { 
+document.body.style.background="green";
+alert("Вы выбрали зелёный!")
+}
+if (a==4) { 
+document.body.style.background="black";
+alert("Вы выбрали чёрный!")
+}
+if (a==5) { 
+document.body.style.background="rgb(52, 233, 7)";
+alert("Вы выбрали хромокей!")
+}
+if (a==6) { 
+document.body.style.background="rgb(49, 42, 109)";
+alert("Вы выбрали синий!")
+}
+if (a==7) { 
+document.body.style.background="rgb(87, 45, 10)";
+alert("Вы выбрали коричневый!")
+}
+alert("Ваня,никита лошки!!!!!!!!!!!")
+
+let div = document.createElement('div');
+div.className = "msg";
+div.innerHTML = "Некит лох";
+document.body.append(div);
+*/
+var name_0=prompt("Кто Красная шапоска?")
+var name_1=prompt("Кто Бабушка?")
+var name_2=prompt("Кто Волк?")
+var name_3=prompt("Кто Охотник?")
+var name_4=prompt("Кто Расказщик?")
+var name_5=prompt("Кто Мама?")
+let div = document.createElement('div');
+div.className="msg";
+div.innerHTML=`Красная шапочка
+<br>Действуйшие лица:
+<br>Красноя шапочка - ${name_0}
+<br>Бабушка - ${name_1}
+<br>Волк - ${name_2}
+<br>Охотник - ${name_3}
+<br>Расказщик - ${name_4}
+<br>Мама - ${name_5}
+<br>${name_0}Мама можно погулять
+Мне цветов насобирать?
+<br>${name_5}Мама: Ну конечно прогуляйся,
+Да смотри не потеряйся.
+<br>${name_4}Нужно к бабушке сходить
+Пирожками угостить.
+<br>${name_0}Далеко я не уйду.
+Позовешь, и я приду.
+<br>${name_4}несет пирожки в печь. Танец мамы. Мама забирает уже печеные пироги из печи и собирает корзинку.
+<br>${name_5}Мама: Где ты ходишь, егоза?
+Покажись мне на глаза.
+<br>${name_0}Красная Шапочка:: Что, моя мамулечка?
+Мне пора к бабулечке?
+<br>${name_5}Собирайся в путь дорожку
+И возьми с собой лукошко.
+Пироги с капустою
+Получились вкусные.
+<br>${name_5}Бабушке - большой привет.
+Осторожней будь, мой свет.
+<br>${name_4}Вот и в путь пошла она
+В страшный лес совсем одна?
+ совсем не страшный лес
+Очень много в нем чудес.
+да ты что? Не шутишь ты?
+Нет. Танцуют в нем цветы,
+и луна, и звезды.
+В нем легко и просто.`
+document.body.append(div);
+
